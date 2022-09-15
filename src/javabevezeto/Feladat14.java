6ce6f6a2552585d502dea22872be815bb3c63b4a
Rimoczi Daniel <rimoczidani007@gmail.com>
2022-09-15 17:49:07 +0200

    Menü kiíratása

--- src/javabevezeto/Feladat14.java
+++ src/javabevezeto/Feladat14.java
@@ -7,6 +7,17 @@ public class Feladat14 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Kérem adjon meg egy szöveget: ");
         String szoveg = sc.nextLine();
-
+        String menuPont = "";
+        while (!menuPont.equals("f")) {
+            System.out.println("Kérem adja meg mit szeretne kezdeni a szöveggel: ");
+            System.out.println("a - Nagy betűssé alakítan ");
+            System.out.println("b - Kisbetűssé alakítani ");
+            System.out.println("c - Lekérdezni a hosszát ");
+            System.out.println("d - Összehasonlítani egy másik stringel (string2 bekérése) ");
+            System.out.println("e - Egy részét kiiratni a szövegnek");
+            System.out.println("f - kilépés");
+            System.out.print("Kérem adja meg az elvégzendő müvelet betüjelét: ");
+            menuPont = sc.nextLine().toLowerCase();
+        }
     }
 }
